020bba908c1fd9af20a434186d3e8a4d2f5c1d75
Daksh1802 <66068234+Daksh1802@users.noreply.github.com>
2020-06-16 17:15:32 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class ground{
+constructor(x,y,w,h){
+    var option={
+isStatic:true 
+
+    }
+this.body=Bodies.rectangle(x,y,w,h,option)
+World.add(world,this.body);
+this.width=w
+this.h=h
+}
+display(){
+rectMode(CENTER);
+rect(this.body.position.x,this.body.position.y,this.width,this.h);
+}
+
+
+}
